df04522e450e42b1a1837d115eda88a40ac85739
Robert Hintz <roberthintz@gmail.com>
2014-02-13 21:32:30 -0800

    Add way of getting resolution

--- start_session.sh
+++ start_session.sh
@@ -10,12 +10,15 @@ HOST=$(hostname)
 echo $HOST
 SUBSTRING="7750"
 SUBSTRING2="meowmixwebsites"
+RESOLUTION_SIZE=`xdpyinfo | grep dimensions | cut -d ' ' -f 7`
 
 if echo "$HOST" | grep -q "$SUBSTRING"; then
     echo "${SUBSTRING} was found in ${HOST}"
+    echo "${RESOLUTION_SIZE}"
     avconv -f x11grab -r 4 -s 3840x1080 -i :0.0 -vcodec libx264 -preset ultrafast -threads 4 -y $1.mp4
 elif echo "$HOST" | grep -q "$SUBSTRING2"; then
     echo "${SUBSTRING} is not in ${HOST}, but ${SUBSTRING2} is in ${HOST}"
+    echo "${RESOLUTION_SIZE}"
     avconv -f x11grab -r 4 -s 1366x768 -i :0.0 -vcodec libx264 -preset ultrafast -threads 4 -y $1.mp4
 else
     echo "No Proper Computer Choices Found. Please Add your Computer Settings to the File"
